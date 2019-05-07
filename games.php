49a68b9163ebe8fc6ec1c93c327d1e0383c039f0
kenl5 <kenl5@uci.edu>
2019-05-06 19:56:16 -0700

    complete

---
+++ games.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link type="text/css" rel="stylesheet" href="css/carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+      
+        
+    </head>
+    <body>
+        <img src="image/logo.jpg"  style="width:360px;height:100px;text-align:center;position:relative;left:2%"> 
+
+        <nav class="col-md-12 col-xs-12">
+                <ul>
+                    <li><a href='index.html'>News</a></li>
+                    <li class='active'><a href='games.php'>Games</a></li>
+                    <li><a href='more.html'>About us</a></li>
+
+                    
+                    <li><b>Cart</b></li>
+                    <li><b>Account</b></li>
+                    
+                    
+                </ul>
+                        <div class="clear"></div>
+        </nav>
+        <div class="col-md-12 col-xs-12" style="background-image: url('https://borderlands.com/images/home/footer-bg.jpg?eae91b1d47e12e7f0c62f5bfaf5bca65')">
+        
+
+            
+            <div id="wrapper"> 
+                <img src="image/rec.png"  style="width:100%;height:100%;text-align:center;position:relative;">
+            </div>
+            <div id="wrapper"> 
+        <div>
+            
+        <table class="lib" cellspacing="100">
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "lk824651233";
+            $dbname = "gamedb";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+            } 
+
+            $sql = "SELECT id, name, price, platform, img FROM game";
+            $result = $conn->query($sql);
+            if ($result->num_rows >0){
+                while($row = $result->fetch_assoc()){
+                    echo "<tr>";
+                    echo "<th><div><img src=image/".$row["img"]." width=\"200\" height=\"200\"></th>";
+                    echo "<th><div><h4><pre>    ".$row["name"]."</pre></h4>";
+                    echo "<br><pre>      Game ID: ".$row["id"];
+                    echo "<br>      Price: $".$row["price"];
+                    echo "<br>      Platform: ".$row["platform"]."</pre></th>";
+                    echo "<th><pre>   <a target=\"_blank\" href=\"purchase.php?name=".$row["name"]."\"><img src=\"image/purchase.png\" width=\"100\" height=\"100\"></a></pre></th>";
+                    echo "</tr>";
+                }
+            }
+
+
+
+            $conn->close();
+            ?> 
+            
+        </table>
+
+        </div>
+        </div>
+            <div id="wrapper"> 
+                <img src="image/ps4bar.png"  style="width:100%;height:100%;text-align:center;position:relative;">
+            </div>
+            <div id="wrapper"> 
+                <img src="image/ps4.png"  style="width:100%;height:100%;text-align:center;position:relative;">
+            </div>
+        <br>
+        
+        
+            
+        <section class="console">
+            <table style="width:100%">
+            </table>
+        </section>
+                
+            
+        </div>
+    </body>
+</html>
+
--- welcome_get.php
+++ welcome_get.php
@@ -1,79 +1,92 @@
 <html>
 <head>
-        <title>TODO supply a title</title>
+        <title>Confirmation</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="style.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link type="text/css" rel="stylesheet" href="css/carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 <body>
+    <img src="image/logo.jpg"  style="width:360px;height:100px;text-align:center;position:relative;left:2%"> 
+        
+        <nav class="col-md-12 col-xs-12" >
+            <ul>
+                <li><a href='index.html'>News</a></li>
+                <li><a href='games.php'>Games</a></li>
+                <li class='active'><a href='more.html'>About us</a></li>
 
-        <div id="wrapper"> 
 
+                <li><b>Cart</b></li>
+                <li><b>Account</b></li>
+
+
+            </ul>
+                    <div class="clear"></div>
+        </nav>
+        
+    <div style="background-image: url('https://borderlands.com/images/home/footer-bg.jpg?eae91b1d47e12e7f0c62f5bfaf5bca65')">
+       <div id="wrapper">  
 <?php
-define('DB_NAME', 'hw2');
+define('DB_NAME', 'gamedb');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'inf124');
+define('DB_PASSWORD', 'lk824651233');
 define('DB_HOST', 'localhost');
 
-$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
 if (!link){
 	die('Could not connect: '.mysql_error());
 }
 
-$db_selected = mysql_select_db(DB_NAME, $link);
+$db_selected = mysqli_select_db($link, DB_NAME);
 
 if (!$db_selected){
-	die('Cant use'.DB_NAME.':'.mysql_error());
+    die('Can\'t use'.DB_NAME.':'.mysql_error());
 }
 
-$value1 = $_GET["Itemnumber"];
-$value2 = $_GET["quality"];
+$value1 = $_GET["ItemName"];
+$value2 = $_GET["quantity"];
 $value3 = $_GET["FirstName"];
 $value4 = $_GET["LastName"];
 $value5 = $_GET["Phonenumber"];
-$value6 = $_GET["e-mail"];
+$value6 = $_GET["email"];
 $value7 = $_GET["Address"];
 $value8 = $_GET["Cardnumber"];
 $value9 = $_GET["Shippingmethod"];
 $value10 = $_GET["state"];
 
-
 $cof = 0;
 
+$sql = "INSERT INTO purchase (ItemName,quantity,FirstName,LastName,Phonenumber,email,Address,Cardnumber,Shippingmethod,state) VALUES ('$value1','$value2','$value3','$value4','$value5','$value6','$value7','$value8','$value9','$value10')";
 
 
-
-
-
-$sql = "INSERT INTO purchase (item,num,FN,LN,phone,mail,address,card,ship,state) VALUES ('$value1','$value2','$value3','$value4','$value5','$value6','$value7','$value8','$value9','$value10')";
-
-
-if (mysql_query($sql)){
-	echo "<h2>Thank you for your order</h2><br>";
+if (mysqli_query($link, $sql)){
+	echo "<pre><h2>Thank you for your order</h2></pre><br>";
 	echo "<h2>Purchase information</h2>";
-	echo "Itemnumber    :".$value1."<br>";
-	echo "quality       :".$value2."<br>";
+	echo "<pre>Game name: ".$value1."</pre><br>";
+	echo "<pre>Quantity: ".$value2."</pre><br>";
 	echo "<h2>Contact information</h2>";
-	echo "FirstName    :".$value3."<br>";
-	echo "LastName    :".$value4."<br>";
-	echo "Phonenumber    :".$value5."<br>";
-	echo "e-mail    :".$value6."<br>";
-	echo "Address    :".$value7."<br>";
-	echo "State    :".$value10."<br>";
+	echo "<pre>FirstName: ".$value3."</pre><br>";
+	echo "<pre>LastName: ".$value4."</pre><br>";
+	echo "<pre>Phonenumber: ".$value5."</pre><br>";
+	echo "<pre>e-mail: ".$value6."</pre><br>";
+	echo "<pre>Address: ".$value7."</pre><br>";
+	echo "<pre>State: ".$value10."</pre><br>";
 	echo "<h2>Payment information</h2>";
-	echo "Cardnumber    :".$value8."<br>";
-	echo "Shippingmethod    :".$value9."<br>";
-
-
-
-
-	
-
+	echo "<pre>Cardnumber: ".$value8."</pre><br>";
+	echo "<pre>Shippingmethod: ".$value9."</pre><br>";
+}else{
+    echo "ERROR: Could not execute $sql. " . mysqli_error($link);
 }
 
-mysql_close();
-?>
 
+mysqli_close($link);
+?>
+        
+    </div>
+<br><br><br><br>
+    </div>
 </body>
 </html>
